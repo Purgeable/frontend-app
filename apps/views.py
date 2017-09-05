bea9e7bd6b84e57934ed4a3e8eedc1407ef1e668
Adi Bibosunoff <pythonista8@gmail.com>
2017-09-05 15:27:56 +0430

    Render CSV values

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template_string
+from .df_values import dfa, dfm, dfq
 
 # Define the blueprint for this application
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def fetch_csv():
-    return render_template_string("<h1>hi there</h1>")
+@main.route('/annual/')
+def annual():
+    content = dfa.to_csv()
+    return render_template_string(content)
+
+@main.route('/quarterly/')
+def quarterly():
+    content = dfq.to_csv()
+    return render_template_string(content)
+
+@main.route('/monthly/')
+def monthly():
+    content = dfm.to_csv()
+    return render_template_string(content)
