ee49c15f05bd7d4aa59aa38dc3ec2273af329b83
Adi Bibosunoff <pythonista8@gmail.com>
2017-09-05 14:54:56 +0430

    Change main view text

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template_string
 
 # Define the blueprint for this application
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def fetch_csv():
-    raise Exception("it works!")
+    return render_template_string("<h1>hi there</h1>")
