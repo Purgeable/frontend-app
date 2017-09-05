833baa4dd2e4a42f86ecdb6a3e8c720b25fc5ca4
Adi Bibosunoff <pythonista8@gmail.com>
2017-09-05 14:49:46 +0430

    Enable debug mode

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 # Statement for enabling the development environment
-DEBUG = False
+DEBUG = True
 
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
