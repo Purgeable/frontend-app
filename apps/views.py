51328652c68e2320053452f73ca10d0c5bb72f14
Adi Bibosunoff <pythonista8@gmail.com>
2017-09-05 18:09:55 +0430

    Add status checking and webhook sample

--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template_string, request
+import json
+import subprocess
+
+from flask import Blueprint, render_template_string, request, jsonify
 from .df_string_proxies import dfa_text, dfm_text, dfq_text
 
 # Define the blueprint for this application
@@ -19,8 +22,20 @@ def monthly():
     content = dfm_text
     return render_template_string(content)
 
+@main.route('/status/')
+def check_status():
+    with open('status.json') as f:
+        json_data = json.loads(f.read())
+        f.close()
+    return jsonify(json_data)
+
 @main.route('/webhook/', methods=['POST'])
 def webhook():
     """Receive payload from GitHub webhook then run tests."""
     json_data = request.get_json()
-    raise Exception("RECEIVED", json_data)
+    exit_code = subprocess.call(['pytest'], shell=True)
+    with open('status.json', 'w') as f:
+        status = 'success' if exit_code == 0 else 'error'
+        f.write('{"status": "%s"}' % status)
+        f.close()
+    return render_template_string("")
