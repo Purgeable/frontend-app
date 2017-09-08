bf8e4df539e4272f4b7e1617e780d876e671a0eb
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-09-08 11:13:29 +0300

    class refactroing

--- apps/classes.py
+++ apps/classes.py
@@ -1,52 +1,43 @@
-import pandas as pd
+import os
 from urllib import request
 
-class CSVFile(object):
-    """Class that controls a CSV file."""
+
+def get_parent_repo_url(filename):
+    url = "https://raw.githubusercontent.com/epogrebnyak/mini-kep/master/data/processed/latest/{}"
+    return url.format(filename) 
+
+def download_file_contents(url):
+    return request.urlopen(url).read().decode('utf-8')
+    
+
+class LocalFile(object):
+    """Class that controls a local CSV file."""
     def __init__(self, filename, dirname='files'):
-        self.filename = filename
-        self.dirname = dirname
-        self.url = 'https://raw.githubusercontent.com/epogrebnyak/mini-kep/master/data/processed/latest/%s' % \
-            filename
+        self.path = os.path.join(dirname, filename)
+        self.url = get_parent_repo_url(filename)            
 
-    def _open_local(self, mode):
-        """Return a local file instance."""
-        return open("%s/%s" % (self.dirname, self.filename), mode)
-
-    def update(self):
+    def update_from_parent_repo(self):
         """Update a local copy with the latest content from the main
-        GitHub repository.
+           GitHub repository.
         """
-        with self._open_local('w') as csv:
-            data = request.urlopen(self.url).read().decode('utf-8')
+        data = download_file_contents(self.url)
+        with open(self.path, 'w') as csv:
             csv.write(data)
 
     def get_contents(self):
         """Return contents of a local copy."""
-        with self._open_local('r') as csv:
+        with open(self.path, 'r') as csv:
             contents = csv.read()
         return contents
+    
+    def __eq__(self. x):
+        return self.get_contents() == x.get_contents()
 
-class DataFrame(object):
-    """Class that manipulates a pandas dataframe."""
-    def _make_url(self, freq):
-        url = "https://raw.githubusercontent.com/epogrebnyak/mini-kep/master/data/processed/latest/{}"
-        filename = "df{}.csv".format(freq)
-        return url.format(filename)
+class RemoteFile(object):
+    """Class that controls a local CSV file."""
+    def __init__(self, filename):
+        self.url = get_parent_repo_url(filename)
 
-    def get_from_repo(self, freq):
-        """Suggested code to read pandas dataframes from 'mini-kep'
-        stable URL.
-        """
-        url = self._make_url(freq)
-        return self.read_csv(url)
-
-    def read_csv(self, source):
-        """Canonical wrapper for pd.read_csv(). Treats first column at
-        time index.
-
-        Returns:
-            pd.DataFrame()
-        """
-        converter_arg = dict(converters={0: pd.to_datetime}, index_col=0)
-        return pd.read_csv(source, **converter_arg)
+    def get_contents(self):
+        """Return contents of a remote file."""
+        return download_file_contents(self.url)    
--- apps/views.py
+++ apps/views.py
@@ -1,69 +1,52 @@
 import json
 import datetime
 import subprocess
-import markdown2 as md
+#import markdown2 as md
 
-from flask import Blueprint, render_template_string, render_template, \
-                  jsonify, Markup
-from .classes import CSVFile
+from flask import (Blueprint, render_template_string, render_template,
+                   jsonify, Markup)
+from .classes import LocalFile, RemoteFile
 
 # Define the blueprint for this application
 main = Blueprint('main', __name__, template_folder='../templates')
 
 @main.route('/')
 def home():
+    #FIXME: maybe pygments can go here too?
     md_extras = ['fenced-code-blocks']
     source = render_template('home.html')
     contents = Markup(md.markdown(source, extras=md_extras))
     return render_template_string(contents)
 
-@main.route('/annual/')
-def annual():
-    csv_file = CSVFile('dfa.csv')
+def render_file(filename):
+    csv_file = LocalFile(filename)
     contents = csv_file.get_contents()
     return render_template_string(contents)
+
+@main.route('/annual/')
+def annual():
+    return render_file('dfa.csv')
 
 @main.route('/quarterly/')
 def quarterly():
-    csv_file = CSVFile('dfq.csv')
-    contents = csv_file.get_contents()
-    return render_template_string(contents)
+    return render_file('dfq.csv')
 
 @main.route('/monthly/')
 def monthly():
-    csv_file = CSVFile('dfm.csv')
-    contents = csv_file.get_contents()
-    return render_template_string(contents)
+    return render_file('dfmq.csv')
 
 @main.route('/status/')
 def check_status():
     with open('status.json') as f:
         json_data = json.loads(f.read())
-        f.close()
     return jsonify(json_data)
 
+# Not used - to depreciate ----------------------------------------------------                
 class ExitCodeDoesNotExist(Exception):
     """Raise when pytest exit code is unknown."""
     pass
 
-  
-# FIXME:
-def check_csv_identity(): 
 
-            sources = {
-                'a': 'gitub address string'
-                'q': 
-                'm': 
-            }
-            for freq, url in sources.items():
-                # reloading a DataFrame method catches an eye to pandas user, 
-                # but can be OK for 
-                df = DataFrame()
-                df_repo = df.get_from_repo(freq)
-                df_app = df.read_csv(url)
-                assert df_repo.equals(df_app)
-
-# Not used -------------------------------------------------------------------                
 def get_pytest_integration_test_result():  
     """Return pytest result and string comment."""
     comments = {
@@ -82,35 +65,44 @@ def get_pytest_integration_test_result():
          raise ExitCodeDoesNotExist("pytest exited with an unknown code")   
  # end  -----------------------------------------------------------------------
   
+def check_csv_identity(): 
+    flags = []
+    for filename in ['dfa.csv', 'dfq.csv', 'dfm.csv']:
+        # this will use get_contents()
+        flag = (LocalFile(filename) == RemoteFile(filename))
+        flags.append(flag)
+    return all(flags)
+  
+def from_json(filename):
+    with open(filename, 'r') as f:
+        content = f.read()
+    return json.loads(content)
+    
+def to_json(what, filename):
+    # Save test results to status.json
+    with open(filename, 'w') as f:
+        content = json.dumps(what)
+        f.write(content)        
+    
+
 @main.route('/webhook/', methods=['POST'])
 def webhook():
-    """Receive payload from GitHub webhook then run tests."""
-    
-    #EP: we had to update files first before test! ;)
+    """Receive payload from GitHub webhook and re-check file identity."""
+   
+    #EP: we had to update files first before test! Not after it! ;))
     
     # Update local copies with the latest data
     filenames = ['dfa.csv', 'dfm.csv', 'dfq.csv']
     for name in filenames:
-        csv_file = CSVFile(name)
-        csv_file.update()
+        csv_file = LocalFile(name)
+        csv_file.update_from_parent_repo()
     
-    #EP:
-    # FIXME: must get a bool on identity of the dataframes
-    is_updated = True    
+    # get a bool on identity of the files
+    is_updated_ok = check_csv_identity()    
     
-    # Save test results to status.json
-    with open('status.json', 'w') as json_file:
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        body = {
-            'timestamp': now,
-            'is_updated': is_updated
-            '' 
-            #'pytest_exit_code': exit_code,
-            #'is_validated': exit_code == 0,
-            #'comment': comment
-        }
-        json_file.write(json.dumps(body))
-        json_file.close()
-
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    status_dict = {'timestamp': now,
+                   'is_updated_ok': is_updated_ok}
+    to_json(status_dict, 'status.json')
 
     return render_template_string("")
