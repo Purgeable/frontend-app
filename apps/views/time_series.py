cb2acdc9a947a894414969b4e2c7c0e0b27cc8bd
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-09-15 17:41:54 +0300

    Update time_series.py

--- apps/views/time_series.py
+++ apps/views/time_series.py
@@ -4,21 +4,20 @@ from apps.helpers.url_decomposer import decompose_inner_path
 # Define the blueprint for this application
 ts = Blueprint('time_series', __name__)
 
-BASE_URL = '/<string:domain>/<series>/<string:varname>'
+BASE_URL = '/<string:domain>/series/<string:varname>'
 
 @ts.route(BASE_URL)
-def indicator_homepage(domain, series, varname):
+def indicator_homepage(domain, varname):
     """Returns html view with latest values, chart and download instructions."""
     ctx = {
         'domain': domain,
-        'series': series,
         'varname': varname
     }
     return render_template('ts_landing.html', **ctx)
 
 @ts.route(f'{BASE_URL}/<string:freq>/<path:inner_path>')
-def time_series_api(domain, series, varname, freq, inner_path):
-    """Decompose incoming URL call into json"""
+def time_series_api_interface(domain, varname, freq, inner_path):
+    """Decompose incoming URL into API request."""
     
     #FIXME: need exception invoker for this 
     if freq not in 'dwmqa':
@@ -28,7 +27,6 @@ def time_series_api(domain, series, varname, freq, inner_path):
     # ---------------
     ctx = {
         'domain': domain,
-        'series': series,
         'varname': varname,
         'frequency': freq
     }
